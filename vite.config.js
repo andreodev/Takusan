d3770b40188c514fe1749a9f4aa03a6e181f29f8
andreodev <developerandreo@gmail.com>
2025-07-10 02:11:22 -0300

    deploy 02

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // para dev
+    historyApiFallback: true,
+  },
+  // para build (muito importante para deploy)
+  base: '/',
 })
